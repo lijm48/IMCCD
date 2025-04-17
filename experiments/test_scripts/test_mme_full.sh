c132fc7b7d8143c84325b5f7ead0c905521449f7
lijm48 <lijm48@mail2.sysu.edu.cn>
2025-04-17 20:46:52 +0800

    update

--- experiments/test_scripts/test_mme_full.sh
+++ experiments/test_scripts/test_mme_full.sh
@@ -4,22 +4,22 @@ model="llava15"
 seed=42
 
 
-python ./eval/eval_mme.py \
---eval_type full \
---results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_baseline_seed${seed}/mme_answers 
-
-
-python ./eval/eval_mme.py \
---eval_type full \
---results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_vcd_seed${seed}/mme_answers
-
-
-python ./eval/eval_mme.py \
---eval_type full \
---results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_icd_seed${seed}/mme_answers
+# python ./eval/eval_mme.py \
+# --eval_type full \
+# --results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_baseline_seed${seed}/mme_answers 
 
 
 # python ./eval/eval_mme.py \
 # --eval_type full \
-# --results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_imccd_lowangle_alpha05_seed42/mme_answers
+# --results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_vcd_seed${seed}/mme_answers
+
+
+# python ./eval/eval_mme.py \
+# --eval_type full \
+# --results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_icd_seed${seed}/mme_answers
+
+
+python ./eval/eval_mme.py \
+--eval_type full \
+--results_dir ./output/${dataset_name}/${model}_${dataset_name}_answers_imccd_seed42/mme_answers
 
