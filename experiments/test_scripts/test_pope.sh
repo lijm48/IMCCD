9db52a0d868198298cf45cbb1f2d760d3f4678f7
lijm48 <lijm48@mail2.sysu.edu.cn>
2025-04-27 10:31:23 +0800

    debug

--- experiments/test_scripts/test_pope.sh
+++ experiments/test_scripts/test_pope.sh
@@ -14,7 +14,6 @@ seed=42
 sampling="sample"
 
 results_file="${model}_${dataset_name}_pope_${type}_answers_imccd_seed${seed}_${sampling}.jsonl"
-results_file="qwenvl_gqa_pope_popular_answers_imccd_imccd_seed42_sample.jsonl"
 
 
 echo ${results_file}
